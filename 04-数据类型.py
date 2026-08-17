28a9ae3f99cc301ac0ee84024e18bca571882932
SONG5277 <3314350233@qq.com>
2026-08-17 23:17:34 +0800

    04-数据类型

---
+++ 04-数据类型.py
@@ -0,0 +1,27 @@
+#字符串
+#变量名 = "内容"
+#变量名 = '内容'
+text1="我是一个字符串1"
+text2='我是一个字符串2'
+print(text1)
+print(text2)
+print(len(text1))#len函数，获取这段文本有多少字符
+print(len(text2))
+
+#数值类型
+#两种 int 或 float ，会自动检测数值数据是int或float
+num1=1 #int
+num2=1.0 #float
+
+#布尔类型 only两个值 真 或 假
+#Ture（1） 或 False（0）  通常进行判断，首字母大写
+#其实是整数的子类
+
+#空值
+fruits=None
+#表示还没有赋值
+
+#复数
+#变量 = complex(实部, 虚部)
+a=complex(4,5)
+print(a)
